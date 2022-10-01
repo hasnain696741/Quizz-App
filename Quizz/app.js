232ae5846b0d5680824409a83dbbbd9cc7fed77a
Muhammad Hasnain <110912556+hasnain696741@users.noreply.github.com>
2022-10-01 00:54:22 -0700

    Add files via upload

---
+++ Quizz/app.js
@@ -0,0 +1,23 @@
+
+
+function btn(){
+    
+    var opt1 = document.getElementById('four')
+    correctans:{
+        opt1
+    }
+    
+    var opt2 = document.getElementById('five')
+    var opt3 = document.getElementById('six')
+    var opt4 = document.getElementById('seven')
+
+    if( opt1.checked == true ){
+       
+        alert('correct')
+    }
+   else{
+    alert('wrong')
+   }
+
+
+}
